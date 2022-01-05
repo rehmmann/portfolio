72a63b1d889d40e9e29fe5dbcfe9ae59f9810edd
rehmmann <rehmansajid999@gmail.com>
2022-01-05 13:12:16 +0800

    Add project tabs

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,8 +3,16 @@ import { makeStyles } from "@material-ui/core/styles";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import PropTypes from "prop-types";
+import * as React from "react";
+
 const useStyles = makeStyles({
-  root: { backgroundColor: "#ebeeee" },
+  root: {
+    backgroundColor: "#ebeeee",
+  },
   projects: {
     display: "flex",
     flexDirection: "row",
@@ -19,9 +27,56 @@ const useStyles = makeStyles({
     height: 400,
     width: 500,
   },
+  tabs: {
+    color: "#151515",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingBottom: 100,
+  },
 });
+
 function Projects() {
   const classes = useStyles();
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className={classes.root}>
       <Divider variant="middle" />
@@ -33,59 +88,85 @@ function Projects() {
       >
         Projects
       </Typography>
-      <div className={classes.projects}>
-        <Container className={classes.slider}>
-          <Typography
-            className={classes.headerText}
-            variant="h6"
-            align="center"
+      <Box
+        className={classes.root}
+        sx={{
+          flexGrow: 1,
+          // bgcolor: "background.paper",
+          display: "flex",
+          justifyContent: "center",
+          // color: "#FF5C5C",
+        }}
+      >
+        <div className={classes.tabs}>
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            textColor="inherit"
+            indicatorColor=""
+            sx={{ borderRight: 1, borderColor: "divider" }}
           >
-            Pepper
-          </Typography>
-          <Slider
-            dots={true}
-            autoplaySpeed={2000}
-            autoplay={true}
-            arrows={true}
-            pauseOnHover={true}
-            speed={500}
-            slidesToShow={3}
-            slidesToScroll={2}
-            focusOnSelect={true}
-            // fade={true}
-            // centerMode={true}
-          >
-            <img alt="" height={300} src={"Pepper1.jpg"}></img>
-            <img alt="" height={300} src={"Pepper2.jpg"}></img>
-            <img alt="" height={300} src={"Pepper3.jpg"}></img>
-            <img alt="" height={300} src={"Pepper4.jpg"}></img>
-            <img alt="" height={300} src={"Pepper5.jpg"}></img>
-            <img alt="" height={300} src={"Pepper6.jpg"}></img>
-          </Slider>
-        </Container>
-        <Container className={classes.slider}>
-          <Typography
-            className={classes.headerText}
-            variant="h6"
-            align="center"
-          >
-            Union
-          </Typography>
-          <Slider
-            dots={true}
-            // autoplaySpeed={2000}
-            // autoplay={true}
-            // arrows={true}
-            // pauseOnHover={true}
-            // speed={500}
-            // slidesToShow={3}
-            // slidesToScroll={2}
-            // focusOnSelect={true}
-          >
-            <img alt="" height={300} src={"Union1.jpg"}></img>
-          </Slider>
-        </Container>
-      </div>
+            <Tab label="Pepper" {...a11yProps(0)} />
+            <Tab label="Union" {...a11yProps(1)} />
+            {/* <Tab label="Item Three" {...a11yProps(2)} /> */}
+          </Tabs>
+        </div>
+
+        <TabPanel value={value} index={0}>
+          {/* <div className={classes.projects}> */}
+          <Container className={classes.slider}>
+            <Slider
+              dots={true}
+              autoplaySpeed={2000}
+              autoplay={true}
+              arrows={true}
+              pauseOnHover={true}
+              speed={500}
+              slidesToShow={3}
+              slidesToScroll={2}
+              focusOnSelect={true}
+              // fade={true}
+              // centerMode={true}
+            >
+              <img alt="" height={315} src={"Pepper1.jpg"}></img>
+              <img alt="" height={315} src={"Pepper2.jpg"}></img>
+              <img alt="" height={315} src={"Pepper3.jpg"}></img>
+              <img alt="" height={315} src={"Pepper4.jpg"}></img>
+              <img alt="" height={315} src={"Pepper5.jpg"}></img>
+              <img alt="" height={315} src={"Pepper6.jpg"}></img>
+            </Slider>
+          </Container>
+          {/* </div> */}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Container className={classes.slider}>
+            {/* <Typography
+              className={classes.headerText}
+              variant="h6"
+              align="center"
+            >
+              Union
+            </Typography> */}
+            <Slider
+              dots={true}
+              // autoplaySpeed={2000}
+              // autoplay={true}
+              // arrows={true}
+              // pauseOnHover={true}
+              // speed={500}
+              // slidesToShow={3}
+              // slidesToScroll={2}
+              // focusOnSelect={true}
+            >
+              <img alt="" height={315} src={"Union1.jpg"}></img>
+            </Slider>
+          </Container>
+        </TabPanel>
+      </Box>
+      <div className={classes.projects}></div>
     </div>
   );
 }
